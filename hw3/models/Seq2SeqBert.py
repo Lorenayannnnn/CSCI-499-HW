f2218cb8b02f1d82b791c5899852a94a99acf881
Lorenayannnnn <lorenayan@qq.com>
2022-11-14 09:18:33 -0800

    init Seq2SeqBert

--- hw3/models/Seq2SeqBert.py
+++ hw3/models/Seq2SeqBert.py
@@ -2,10 +2,15 @@ import torch.nn as nn
 from transformers import BertGenerationEncoder, BertGenerationDecoder, EncoderDecoderModel, BertTokenizer, AutoTokenizer
 
 
+# Reference:
+# https://huggingface.co/docs/transformers/model_doc/encoder-decoder
+# https://huggingface.co/docs/transformers/model_doc/bert-generation
+
 class Seq2SeqBert(nn.Module):
     """
     Seq2Seq model based on Bert
     """
+
     def __init__(self, model_name: str, input_bos_token_id: int, input_eos_token_id: int, output_bos_token_id: int,
                  output_eos_token_id: int):
         super(Seq2SeqBert, self).__init__()
@@ -17,16 +22,18 @@ class Seq2SeqBert(nn.Module):
         self.bert2bert = EncoderDecoderModel(encoder=self.encoder, decoder=self.decoder)
         self.tokenizer = BertTokenizer.from_pretrained(model_name)
 
-    def forward(self, episodes, labels):
+    def forward(self, episodes, action_labels, target_labels):
         """
         parameters:
         - episodes: [batch_size, seq_len]
-        - labels: [batch_size, num_of_instruction_in_one_episode, 2(containing action and target)]
-        - seq_lens [batch_size] (store how long each episode instruction is)
+        - action_labels: [batch_size, num_of_instruction_in_one_episode]
+        - target_labels: [batch_size, num_of_instruction_in_one_episode]
 
-        return:
-        - all_predicted_actions ([batch_size, instruction_num])
-        - all_predicted_targets ([batch_size, instruction_num])
-        - action_prob_dist ([batch_size, instruction_num, n_actions])
-        - target_prob_dist ([batch_size, instruction_num, n_targets])
+        return: https://huggingface.co/docs/transformers/v4.24.0/en/main_classes/output#transformers.modeling_outputs.Seq2SeqLMOutput
+        action_outputs: transformers.modeling_outputs.Seq2SeqLMOutput
+        target_outputs: transformers.modeling_outputs.Seq2SeqLMOutput
         """
+        action_outputs = self.bert2bert(input_ids=episodes, decoder_input_ids=action_labels, labels=action_labels)
+        target_outputs = self.bert2bert(input_ids=episodes, decoder_input_ids=target_labels, labels=target_labels)
+
+        return action_outputs, target_outputs
